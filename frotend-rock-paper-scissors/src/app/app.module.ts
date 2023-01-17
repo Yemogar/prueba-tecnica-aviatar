b031b13be586cc0ed151d6aee85b70949243b209
Yeray Molina Garcia <yemogar@gmail.com>
2023-01-18 00:14:49 +0100

    Finish login and register pages

--- frotend-rock-paper-scissors/src/app/app.module.ts
+++ frotend-rock-paper-scissors/src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './features/login/login.component';
-import { RegisterComponent } from './features/register/register.component';
+import { AuthenticationModule } from './authentication/authentication.module';
 import { MenuComponent } from './features/menu/menu.component';
-import { GameComponent } from './features/game/game.component';
 import { StatisticsComponent } from './features/statistics/statistics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     MenuComponent,
-    GameComponent,
     StatisticsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AuthenticationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frotend-rock-paper-scissors/src/app/authentication/authentication.module.ts
+++ frotend-rock-paper-scissors/src/app/authentication/authentication.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
-    AuthenticationRoutingModule
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatDividerModule
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent
   ]
 })
 export class AuthenticationModule { }
