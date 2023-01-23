64e53f864b2589c2217e4379ca27940f44a28cab
Yeray Molina Garcia <yemogar@gmail.com>
2023-01-23 16:15:47 +0100

    Update folder frontend project

--- frotend-rock-paper-scissor/src/app/app.component.spec.ts
+++ frotend-rock-paper-scissor/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frotend-rock-paper-scissors'`, () => {
+  it(`should have as title 'frotend-rock-paper-scissor'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frotend-rock-paper-scissors');
+    expect(app.title).toEqual('frotend-rock-paper-scissor');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frotend-rock-paper-scissors app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('frotend-rock-paper-scissor app is running!');
   });
 });
--- frotend-rock-paper-scissor/src/app/app.component.ts
+++ frotend-rock-paper-scissor/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'frotend-rock-paper-scissors';
+  title = 'frotend-rock-paper-scissor';
 }
